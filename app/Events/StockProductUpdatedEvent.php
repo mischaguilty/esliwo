925f62ae894297f2f3172975a0eba1c5931c597a
Mischa <go.myke@gmail.com>
2022-01-09 05:13:06 +0200

    otakot

---
+++ app/Events/StockProductUpdatedEvent.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\StockProduct;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class StockProductUpdatedEvent implements ShouldBroadcast
+{
+    use SerializesModels, InteractsWithSockets, Dispatchable;
+
+    private StockProduct $stockProduct;
+
+    public function getStockProduct(): StockProduct
+    {
+        return $this->stockProduct;
+    }
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(StockProduct $stockProduct)
+    {
+        $this->stockProduct = $stockProduct;
+    }
+
+    public function broadcastOn(): PrivateChannel
+    {
+        return new PrivateChannel('stock-products.' . $this->stockProduct->id);
+    }
+}
