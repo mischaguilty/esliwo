985256c1b2cde358bb0f5bd3959dc167c3c13515
Mischa <go.myke@gmail.com>
2022-01-10 21:40:37 +0200

    broadcast use rchannel

--- routes/channels.php
+++ routes/channels.php
@@ -14,6 +14,10 @@
 
 use App\Models\StockProduct;
 
+Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    return (int)$user->id === (int)$id;
+});
+
 \Illuminate\Support\Facades\Broadcast::channel('sporoducts', function ($user) {
     return true;
 });
